464b96617e9a23b1f93450729be92fad048ed4d0
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2019-03-30 14:47:27 +0100

    Remove dead code

--- src/string_util.rs
+++ src/string_util.rs
@@ -4,19 +4,6 @@ pub fn grapheme_count(buf: &str) -> usize {
     UnicodeSegmentation::graphemes(buf, true).count()
 }
 
-#[allow(dead_code)]
-pub fn nth_grapheme_offset(buf: &str, n: usize) -> Option<usize> {
-    for (index, (offset, _)) in
-        UnicodeSegmentation::grapheme_indices(buf, true).enumerate()
-    {
-        if index == n {
-            return Some(offset);
-        }
-    }
-
-    None
-}
-
 pub fn nth_newline_wrapped(
     mut n: usize,
     buf: &str,
@@ -37,18 +24,6 @@ pub fn nth_newline_wrapped(
     }
 
     current_pos
-}
-
-#[allow(dead_code)]
-pub fn last_newline_offset(buf: &str) -> Option<usize> {
-    let mut last = None;
-    for (offset, grapheme) in UnicodeSegmentation::grapheme_indices(buf, true) {
-        if is_newline(grapheme) {
-            last = Some(offset + grapheme_size(grapheme));
-        }
-    }
-
-    last
 }
 
 pub fn nth_last_newline_wrapped(
@@ -74,11 +49,6 @@ pub fn nth_last_newline_wrapped(
     };
 }
 
-#[allow(dead_code)]
-pub fn snd_last_newline_wrapped(buf: &str, screen_width: usize) -> usize {
-    nth_last_newline_wrapped(2, buf, screen_width)
-}
-
 pub fn is_newline(grapheme: &str) -> bool {
     grapheme == "\n" || grapheme == "\r\n"
 }
@@ -101,48 +71,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn byte_offset_simple() {
-        let s = "Hello!";
-        assert_eq!(nth_grapheme_offset(s, 0), Some(0));
-        assert_eq!(nth_grapheme_offset(s, 1), Some(1));
-        assert_eq!(nth_grapheme_offset(s, 2), Some(2));
-        assert_eq!(nth_grapheme_offset(s, 3), Some(3));
-        assert_eq!(nth_grapheme_offset(s, 4), Some(4));
-        assert_eq!(nth_grapheme_offset(s, 5), Some(5));
-        assert_eq!(nth_grapheme_offset(s, 6), None);
-    }
-
-    #[test]
-    fn byte_offset_strange() {
-        let n = "éa";
-        let m = "éa";
-        assert_eq!(n.len(), m.len() + 1);
-        assert_eq!(nth_grapheme_offset(n, 1), Some(n.len() - 1));
-        assert_eq!(nth_grapheme_offset(m, 1), Some(m.len() - 1));
-    }
-
-    #[test]
-    fn test_last_newline_offset() {
-        let s = "a\na";
-        let t = "\naa";
-        let u = "\naa\n";
-        let v = "aa";
-        assert_eq!(last_newline_offset(s), Some(2));
-        assert_eq!(last_newline_offset(t), Some(1));
-        assert_eq!(last_newline_offset(u), Some(4));
-        assert_eq!(last_newline_offset(v), None);
-    }
-
-    #[test]
     fn test_snd_last_newline_wrapped() {
         let s = "\n";
         let t = "";
         let u = "\naa\n";
         let v = "aaaaaa";
-        assert_eq!(snd_last_newline_wrapped(s, 3), 0);
-        assert_eq!(snd_last_newline_wrapped(t, 3), 0);
-        assert_eq!(snd_last_newline_wrapped(u, 3), 1);
-        assert_eq!(snd_last_newline_wrapped(v, 3), 3);
+        assert_eq!(nth_last_newline_wrapped(2, s, 3), 0);
+        assert_eq!(nth_last_newline_wrapped(2, t, 3), 0);
+        assert_eq!(nth_last_newline_wrapped(2, u, 3), 1);
+        assert_eq!(nth_last_newline_wrapped(2, v, 3), 3);
     }
 
     #[test]
